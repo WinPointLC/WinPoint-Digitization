e388dbd289479386e339a630d9db1200aa28938f
Seema Padalkar <seemapadalkar@Seemas-MacBook.local>
2020-06-19 22:05:45 +0530

    Added WorkingProfessionalDetails to batchSchedular

---
+++ WinPointManagementSystem/src/com/winpoint/batchScheduler/fxmlsControllers/WorkingProfessionalDetailsController.java
@@ -0,0 +1,35 @@
+
+	package com.winpoint.batchScheduler.fxmlsControllers;
+
+	import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.winpoint.common.controllers.ParentFXMLController;
+
+    import javafx.event.ActionEvent;
+    import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.image.ImageView;
+
+	public class WorkingProfessionalDetailsController extends ParentFXMLController{
+
+	    @FXML
+	    private ImageView logo;
+
+	    @FXML
+	    private Button cancel;
+
+	    @FXML
+	    void cancelFrame(ActionEvent event) {
+	    	System.out.println(event);
+	    }
+	    @Override
+		public void initialize(URL location, ResourceBundle resources) {
+			// TODO Auto-generated method stub
+			super.initialize(location, resources);
+			logo.setImage(logoImage);
+		}
+		
+
+	}
+
