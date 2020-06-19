9ace6ddfeb16159c37650fa17c0ffa8e0362d274
suhasib1411 <suhasi14buche@gmail.com>
2020-06-19 21:24:16 +0530

    Updated Assignment Records

--- WinPointManagementSystem/src/com/winpoint/batchTracker/fxmlsControllers/AssignmentRecordsScreenController.java
+++ WinPointManagementSystem/src/com/winpoint/batchTracker/fxmlsControllers/AssignmentRecordsScreenController.java
@@ -1,4 +1,5 @@
 package com.winpoint.batchTracker.fxmlsControllers;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -6,51 +7,53 @@ import com.winpoint.common.controllers.ParentFXMLController;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
-import javafx.scene.image.Image;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
-public class AssignmentRecordsScreenController extends ParentFXMLController{
-	  @FXML
-	    private Button back;
+public class AssignmentRecordsScreenController extends ParentFXMLController {
 
-	    @FXML
-	    private Text batchname;
+    @FXML
+    private Button back;
 
-	    @FXML
-	    private Text title;
+    @FXML
+    private Text batchName;
 
-	    @FXML
-	    private ImageView logo;
+    @FXML
+    private Text title;
 
-	    @FXML
-	    private TableColumn<?, ?> name;
+    @FXML
+    private ImageView logo;
 
-	    @FXML
-	    private TableColumn<?, ?> feestatus;
+    @FXML
+    private TableView<?> assignmentsTable;
 
-	    @FXML
-	    private TableColumn<?, ?> courseware;
+    @FXML
+    private TableColumn<?, ?> assignmentsTableNameColumn;
 
-	    @FXML
-	    private TableColumn<?, ?> assignmentsissued;
+    @FXML
+    private TableColumn<?, ?> assignmentsTableFeeStatus;
 
-	    @FXML
-	    private TableColumn<?, ?> assignmentssubmitted;
+    @FXML
+    private TableColumn<?, ?> assignmentsTableCourseware;
 
-	    @FXML
-	    void GoToPreviousScreen(ActionEvent event) {
-	    	System.out.println(event);
-	    }
+    @FXML
+    private TableColumn<?, ?> assignmentsTableAssignmentsIssued;
 
-		@Override
-		public void initialize(URL location, ResourceBundle resources) {
-			// TODO Auto-generated method stub
-			super.initialize(location, resources);
-			logo.setImage(logoImage);
-		}
+    @FXML
+    private TableColumn<?, ?> assignmentsTableAssignmentsSubmitted;
+
+    @FXML
+    void GoToPreviousScreen(ActionEvent event) {
+    	System.out.println(event);
+    }
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		super.initialize(location, resources);
+		logo.setImage(logoImage);
+	}
 
 }
