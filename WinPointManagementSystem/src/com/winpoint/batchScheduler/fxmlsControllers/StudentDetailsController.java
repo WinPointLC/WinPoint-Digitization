cc828a516ca5eb48e22142e1949265174f341431
ak-agarwal <aayush.aka44@gmail.com>
2020-06-25 15:22:32 +0530

    Flow and populating the table

--- WinPointManagementSystem/src/com/winpoint/batchScheduler/fxmlsControllers/StudentDetailsController.java
+++ WinPointManagementSystem/src/com/winpoint/batchScheduler/fxmlsControllers/StudentDetailsController.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.winpoint.common.beans.EnquiryDetails;
 import com.winpoint.common.controllers.ParentFXMLController;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +16,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -28,16 +34,53 @@ public class StudentDetailsController extends ParentFXMLController {
     @FXML
     private Label nameLabel;
     
+    @FXML
+    private TableView<EnquiryDetails> eligibilityTable;
+
+    @FXML
+    private TableColumn<EnquiryDetails, String> courseCol;
+
+    @FXML
+    private TableColumn<EnquiryDetails, String> eligiblityCol;
+
+    
+
+    @FXML
+    private Label collegeLabel;
+
+    @FXML
+    private Label yearLabel;
+
+    @FXML
+    private Label branchLabel;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label dateofEnquiryLabel;
+
+    @FXML
+    private Label contactLabel;
+
+    @FXML
+    private Label suggestionLabel;
+
+    @FXML
+    private Label coursesInterestedLabel;
+
+    
     public void setLabel(String s){ 
     	nameLabel.setText(s);
     }
 
     @FXML
     void cancelClick(ActionEvent event) {
+    	  	
     	FXMLLoader loader = new FXMLLoader();
     	Parent myNewScene;
 		try {
-			myNewScene = loader.load(getClass().getResource("../../batchScheduler/fxmls/StudentDetails.fxml").openStream());
+			myNewScene = loader.load(getClass().getResource("../../common/testClient/FrontScreenFxml.fxml").openStream());
 			Stage stage = (Stage) cancelButton.getScene().getWindow();
 	    	Scene scene = new Scene(myNewScene);
 	    	stage.setScene(scene);
@@ -51,6 +94,20 @@ public class StudentDetailsController extends ParentFXMLController {
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    	collegeLabel.setText("VIT");
+    	yearLabel.setText("B. Tech");
+    	branchLabel.setText("IT");
+    	emailLabel.setText("Aayush@gmail,com");
+    	dateofEnquiryLabel.setText("10.05.20");
+    	contactLabel.setText("9004628901");
+    	suggestionLabel.setText("none");
+    	coursesInterestedLabel.setText("Java Script");
+    	
+    	courseCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+    	eligiblityCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+    	ObservableList<EnquiryDetails>list=FXCollections.observableArrayList(new EnquiryDetails("C", "YES", "", "", ""), new EnquiryDetails("CPP", "YES", "", "", ""));
+    	eligibilityTable.setItems(list);
+    	
     	// TODO Auto-generated method stub
     	super.initialize(location, resources);
     	logo.setImage(logoImage);
--- WinPointManagementSystem/src/com/winpoint/common/testClient/Client.java
+++ WinPointManagementSystem/src/com/winpoint/common/testClient/Client.java
@@ -12,8 +12,8 @@ public class Client extends Application {
 		//System.out.println("Hello");
 		try {			
 		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchTracker/fxmls/LectureScreen.fxml"));
-		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/CoursesName.fxml"));
-		    BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("FrontScreenFxml.fxml"));
+		    BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/CoursesName.fxml"));
+		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("FrontScreenFxml.fxml"));
 		    Scene scene = new Scene(root);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
