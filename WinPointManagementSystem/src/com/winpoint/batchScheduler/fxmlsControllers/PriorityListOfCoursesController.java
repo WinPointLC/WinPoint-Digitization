6ddaf7edbe1fcf3daf54dfc696388c48728b34dd
dixitabhishek239 <dixitabhishek239@icloud.com>
2020-07-01 20:38:47 +0530

    Required small changes are done

--- WinPointManagementSystem/src/com/winpoint/batchScheduler/fxmlsControllers/PriorityListOfCoursesController.java
+++ WinPointManagementSystem/src/com/winpoint/batchScheduler/fxmlsControllers/PriorityListOfCoursesController.java
@@ -1,5 +1,6 @@
 package com.winpoint.batchScheduler.fxmlsControllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,11 +14,15 @@ import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class PriorityListOfCoursesController extends ParentFXMLController{
     @FXML
@@ -30,8 +35,14 @@ public class PriorityListOfCoursesController extends ParentFXMLController{
     
 
     @FXML
-    void cancelFrame(ActionEvent event) {
-   
+    void cancelFrame(ActionEvent event) throws IOException {
+    	FXMLLoader loader = new FXMLLoader();
+    	Parent myNewScene = loader.load(getClass().getResource("../../common/testClient/CategorisedCourseRecommenderFxml.fxml").openStream());
+    	Stage stage = (Stage) cancel.getScene().getWindow();
+    	Scene scene = new Scene(myNewScene);
+    	stage.setScene(scene);
+    	stage.setTitle("Main Scene");
+    	stage.show();
     }
     
     @Override
--- WinPointManagementSystem/src/com/winpoint/common/testClient/Client.java
+++ WinPointManagementSystem/src/com/winpoint/common/testClient/Client.java
@@ -12,7 +12,7 @@ public class Client extends Application {
 		//System.out.println("Hello");
 		try {			
 		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchTracker/fxmls/AttendanceScreen.fxml"));
-		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/EnquiryDetails.fxml"));
+		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/UpdatePage.fxml"));
 			BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("FrontScreenFxml.fxml"));
 		    Scene scene = new Scene(root);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
