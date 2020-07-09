e87135c7f7857d5be74bfd4f796ecd0d2f7b5fa0
GregOratOr <GregOratOr@users.noreply.github.com>
2020-07-09 23:28:57 +0530

    Fees Records Screen Restored
    Associated dao, helper and controllers are updated.

--- WinPointManagementSystem/src/com/winpoint/common/helpers/StudentCourseInstallmentHelper.java
+++ WinPointManagementSystem/src/com/winpoint/common/helpers/StudentCourseInstallmentHelper.java
@@ -2,11 +2,11 @@ package com.winpoint.common.helpers;
 
 import java.util.ArrayList;
 
-import com.winpoint.common.beans.StudentCourseInstallmentDetails;
 import com.winpoint.common.dao.StudentCourseInstallmentDetailsDao;
+import com.winpoint.common.wrappers.FeeRecordsScreenWrapper;
 
 public class StudentCourseInstallmentHelper {
-	public ArrayList<StudentCourseInstallmentDetails> getStudentCourseInstallmentDetailsList() {
-		return new StudentCourseInstallmentDetailsDao().getStudentCourseInstallmentDetails();
+	public ArrayList<FeeRecordsScreenWrapper> getFeeRecordsScreenWrapperList(Integer batchId) {
+		return new StudentCourseInstallmentDetailsDao().getFeeRecordsScreenWrapperList(batchId);
 	}
 }
