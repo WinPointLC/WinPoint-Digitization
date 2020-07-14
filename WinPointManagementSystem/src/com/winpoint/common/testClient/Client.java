e472665ffebbe5c5edf944c2e18a427184f72ca3
ak-agarwal <55729950+ak-agarwal@users.noreply.github.com>
2020-07-14 22:14:14 +0530

    client Changes

--- WinPointManagementSystem/src/com/winpoint/common/testClient/Client.java
+++ WinPointManagementSystem/src/com/winpoint/common/testClient/Client.java
@@ -12,16 +12,12 @@ public class Client extends Application {
 		//System.out.println("Hello");
 		try {			
 		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchTracker/fxmls/AttendanceScreen.fxml"));
-<<<<<<< HEAD
-		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/NumberOfStudents.fxml"));
-		    //BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/EnquiryDetails.fxml"));
+		   
 			BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("FrontScreenFxml.fxml"));
-=======
-		    BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/CoursesName.fxml"));
-			//BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("FrontScreenFxml.fxml"));
->>>>>>> 62d63d13b282f6b4845cfa0ce0e0fb1c25375f95
+//		    BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("../../batchScheduler/fxmls/CoursesName.fxml"));
 		    Scene scene = new Scene(root);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			BorderPane root =(BorderPane) FXMLLoader.load(getClass().getResource("FrontScreenFxml.fxml"));
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
